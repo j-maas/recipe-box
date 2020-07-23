6b929cf1bb30090c93ed2070f8bc5dbeab1296ae
y0hy0h <y0hy0h@gmx.net>
2020-07-23 19:26:10 +0200

    Fuzz Store.PathComponent tests

---
+++ tests/StorePathComponentTest.elm
@@ -0,0 +1,56 @@
+module StorePathComponentTest exposing (suite)
+
+import Dict
+import Expect
+import Fuzz
+import Store.PathComponent as PathComponent
+import Test exposing (..)
+
+
+charsetFuzzer chars =
+    Fuzz.list (Fuzz.oneOf (List.map Fuzz.constant chars))
+        |> Fuzz.map String.fromList
+
+
+validChars =
+    [ 'A', 'B', 'C', 'X', 'Y', 'Z', 'a', 'b', 'c', 'x', 'y', 'z', ' ', '-', '(', ')', '_' ]
+
+
+suite : Test
+suite =
+    describe "path component"
+        [ fuzz (charsetFuzzer validChars) "allows valid characters" <|
+            \name ->
+                PathComponent.autorename name (always False)
+                    |> PathComponent.toString
+                    |> Expect.equal name
+        , fuzz (Fuzz.intRange 1 10) "counts on conflict" <|
+            \conflicts ->
+                let
+                    db =
+                        (( "conflict", () )
+                            :: (List.range 1 conflicts
+                                    |> List.map (\num -> ( "conflict" ++ String.fromInt num, () ))
+                               )
+                        )
+                            |> Dict.fromList
+                in
+                PathComponent.autorename "conflict" (\name -> Dict.member (PathComponent.toString name) db)
+                    |> PathComponent.toString
+                    |> Expect.equal ("conflict" ++ String.fromInt (conflicts + 1))
+        , test "removes special characters" <|
+            \_ ->
+                PathComponent.autorename "remove. these/" (always False)
+                    |> PathComponent.toString
+                    |> Expect.equal "remove these"
+        , test "replaces Umlaute" <|
+            \_ ->
+                PathComponent.autorename "ÄäÖöÜüß" (always False)
+                    |> PathComponent.toString
+                    |> Expect.equal "AeaeOeoeUeuess"
+        , test "removes accents" <|
+            \_ ->
+                PathComponent.autorename "éèàçã" (always False)
+                    |> PathComponent.toString
+                    |> Expect.equal "eeaca"
+        ]
